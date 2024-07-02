a1b21e76fd8998adb36064e614bb03192006b9b1
fahadLine <152792332+fahadLine@users.noreply.github.com>
2024-07-02 17:25:58 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ public class Main {
     Movie movies [] = 
         { 
             new Movie(1, "The matrix", 1999, 8.7, "action"),
-            new Movie(1, "The matrix", 1999, 8.7, "action"),
-            new Movie(1, "The matrix", 1999, 8.7, "action"),
-            new Movie(1, "The matrix", 1999, 8.7, "action"),
-            new Movie(1, "The matrix", 1999, 8.7, "action"),
-            new Movie(1, "The matrix", 1999, 8.7, "action"),
-            new Movie(1, "The matrix", 1999, 8.7, "action"),
-            new Movie(1, "The matrix", 1999, 8.7, "action"),
-            new Movie(1, "The matrix", 1999, 8.7, "action"),
+            new Movie(2, "The Dark Knight", 2008, 9, "action"),
+            new Movie(3, "Terminator 2: Judgment Day", 1991, 8.6, "action"),
 
-        }
+            new Movie(4, "WALL·E", 2008, 8.4, "animation"),
+            new Movie(5, "The lion king", 1994, 8.5, "animation"),
 
-        ;
+            new Movie(6, "Oppenheimer", 2023, 8.3, "drama"),
+            new Movie(7, "Taxi Driver", 1976, 8.2, "drama"),
+
+            new Movie(8, "Bad Boys: Ride or Die", 2024, 7.0, "comedy"),
+            new Movie(9, "Bad Boys", 1995, 6.8, "comedy"),
+        };
 
     public static void main(String[] args) {
 
