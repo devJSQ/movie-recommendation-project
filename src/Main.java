a404b6c8fbb496df43477e0a5c37a4d64071465c
JSQdev <148073798+devJSQ@users.noreply.github.com>
2025-07-15 04:53:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,19 @@
+
 public class Main {
-
-    Movie movies [] = 
-        { 
-            new Movie(1, "The matrix", 1999, 8.7, "action"),
-            new Movie(2, "The Dark Knight", 2008, 9, "action"),
-            new Movie(3, "Terminator 2: Judgment Day", 1991, 8.6, "action"),
-
-            new Movie(4, "WALL·E", 2008, 8.4, "animation"),
-            new Movie(5, "The lion king", 1994, 8.5, "animation"),
-
-            new Movie(6, "Oppenheimer", 2023, 8.3, "drama"),
-            new Movie(7, "Taxi Driver", 1976, 8.2, "drama"),
-
-            new Movie(8, "Bad Boys: Ride or Die", 2024, 7.0, "comedy"),
-            new Movie(9, "Bad Boys", 1995, 6.8, "comedy"),
-        };
-
     public static void main(String[] args) {
+        List<Movie> movieList = Arrays.asList(
+            new Movie(1, "The Matrix", 8.7, 1999, "action"),
+            new Movie(2, "The Dark Knight", 9.0, 2008, "action"),
+            new Movie(3, "Terminator 2: Judgment Day", 8.6, 1991, "action"),
+            new Movie(4, "WALL·E", 8.4, 2008, "animation"),
+            new Movie(5, "The Lion King", 8.5, 1994, "animation"),
+            new Movie(6, "Oppenheimer", 8.3, 2023, "drama"),
+            new Movie(7, "Taxi Driver", 8.2, 1976, "drama"),
+            new Movie(8, "Bad Boys: Ride or Die", 7.0, 2024, "comedy"),
+            new Movie(9, "Bad Boys", 6.8, 1995, "comedy")
+        );
 
-        /* Add your code here */
-        
+        DecisionTree tree = new DecisionTree(movieList);
+        tree.start();
     }
-    
 }
